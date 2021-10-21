3791326480796dbf84717979df8bccae7d198164
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2021-10-21 18:47:12 +0300

    Add files via upload

---
+++ exe7pag38.py
@@ -0,0 +1,16 @@
+S = str(input('Șirul de caractere: '))
+##############################################################################
+print('a) Numarul de aparitii ale caracterului A in sirul S:',S.count('A'))
+######################################################################################
+print('b) Sirul obinut prin substituirea caracterului A print caracterul * :', S.replace("A","*") )
+##########################################################################################
+print('c) Șirul obținut prin radierea din șirul S a tuturor aparițiilor caracterului B:',S.replace("B",""))
+#########################################################################################
+print('d) Numarul de aparitii ale silabei MA in sirul S:',S.count('MA'))
+##################################################################################
+print('e) Sirul obinut prin substituirea silabei MA prin silaba TA :', S.replace("MA","TA") )  
+#####################################################################################
+print('f) Șirul obținut prin radierea din șirul S a tuturor aparițiilor silabei TO:',S.replace("TO",""))
+#############################################################################
+print('g) Scrierea inversa a sirului s:',S[::-1])
+#############################################################################
